aa1f52cd2eb643e1206f45e5ee8caa8e42f67a9b
deniznida <denizunat@hotmail.com>
2015-02-05 14:23:04 -0500

    done

--- spec/intro_to_arrays_spec.rb
+++ spec/intro_to_arrays_spec.rb
@@ -4,20 +4,22 @@ describe "working with arrays" do
     it "creates an empty array and assigns the array to a variable" do
       # create your array here
       # replace the underscore in expect with your variable
-      expect(__).to be_an(Array)
+      my_Array = []
+      expect(my_Array).to be_an(Array)
     end
 
     context 'instantiating an array with 2 objects' do
       # create your array here
-
+      my_Array = [1,2]
       it "is an array" do
         # replace the underscore in expect with your variable
-        expect(__).to be_an(Array)
+        expect(my_Array).to be_an(Array)
       end
 
       it "has 2 objects in it" do
         # replace the underscore in expect with your variable
-        expect(__).to_not eq([])
+        my_Array = ["1", "2"]
+        expect(my_Array).to_not eq([])
       end
     end
   end
@@ -26,22 +28,22 @@ describe "working with arrays" do
     taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
 
     it "returns the first element in the array using its positive index" do
-      answer = __ # replace the underscore with your answer
+      answer = taylor_swift[0] # replace the underscore with your answer
       expect(answer).to eq("Welcome to New York")
     end
 
     it "returns the third element in the array using its positive index" do
-      answer = __ # replace the underscore with your answer
+      answer = taylor_swift[2] # replace the underscore with your answer
       expect(answer).to eq("Style")
     end
 
     it "returns the last element in the array using its negative index" do
-      answer = __ # replace the underscore with your answer
+      answer = taylor_swift[-1] # replace the underscore with your answer
       expect(answer).to eq("Out of The Woods")
     end
 
     it "returns the second element in the array using its negative index" do
-      answer = __ # replace the underscore with your answer
+      answer = taylor_swift[-3] # replace the underscore with your answer
       expect(answer).to eq("Blank Space")
     end
   end
@@ -50,13 +52,13 @@ describe "working with arrays" do
     south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
 
     it 'gets the first element in the array ' do
-      answer = __ # replace the underscore with your answer
+      answer = south_east_asia[0] # replace the underscore with your answer
       expect(answer).to eq("Thailand")
     end
 
 
     it 'gets the last element in the array' do
-      answer = __ # replace the underscore with your answer
+      answer = south_east_asia[-1] # replace the underscore with your answer
       expect(answer).to eq("Myanmar")
     end
   end
@@ -65,7 +67,7 @@ describe "working with arrays" do
   it 'returns the length of the array' do
     programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
 
-    answer = __ # replace the underscore with your answer
+    answer = programming_languages.length # replace the underscore with your answer
     expect(answer).to eq(8)
   end
 
